3bda2b825180f092da5bec15d369b736a2a890a3
Amitava Saha <amitava.bec@gmail.com>
2020-09-15 17:05:58 +0530

    Update log-parser.scala

--- spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
+++ spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
@@ -6,7 +6,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 
 class Utils extends Serializable {
-    val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
 
     def containsIP(line:String):Boolean = return line matches "^([0-9\\.]+) .*$"
     //Extract only IP
@@ -27,20 +26,6 @@ class Utils extends Serializable {
     }
 	
 	
-	def parseLogLine(log: String):
-	LogRecord = {
-		val res = PATTERN.findFirstMatchIn(log) 
-		if (res.isEmpty)
-		{
-			println("Rejected Log Line: " + log)
-			LogRecord("Empty", "", "",  -1 )
-		}
-		else 
-		{
-			val m = res.get
-			LogRecord(m.group(1), m.group(4),m.group(6), m.group(8).toInt)
-		}
-	}
 }
 
 object EntryPoint {
@@ -72,21 +57,5 @@ object EntryPoint {
             println(i)
         }
 		
-		var accessLog = logFile.map(parseLogLine)
-		val accessDf = accessLog.toDF()
-		accessDf.printSchema
-		accessDf.createOrReplaceTempView("nasalog")
-		val output = spark.sql("select * from nasalog")
-		output.createOrReplaceTempView("nasa_log")
-		spark.sql("cache TABLE nasa_log")
-		spark.sql("select url,count(*) as req_cnt from nasa_log where upper(url) like '%HTML%' group by url order by req_cnt desc LIMIT 10").show
-
-		spark.sql("select host,count(*) as req_cnt from nasa_log group by host order by req_cnt desc LIMIT 5").show
-
-		spark.sql("select substr(timeStamp,1,14) as timeFrame,count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt desc LIMIT 5").show
-
-		spark.sql("select substr(timeStamp,1,14) as timeFrame,count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt  LIMIT 5").show
-
-		spark.sql("select httpCode,count(*) as req_cnt from nasa_log group by httpCode ").show
     }
 }
